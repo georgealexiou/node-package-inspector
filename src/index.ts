347c7eac4f13a9f239a5abb77e255eff066d2b27
George Alexiou <george@alexiou.xyz>
2023-02-20 17:21:38 +0000

    Added function to parse node modules and get info

--- src/index.ts
+++ src/index.ts
@@ -5,26 +5,47 @@ type PackageInfo = {
   name: string;
   isDevDependency: boolean;
   currentVersion: string;
-  latestVersion: string;
-  latestVersionDate: Date;
-  currentVersionDate: Date;
+  // latestVersion?: string;
+  // latestVersionDate?: Date;
+  // currentVersionDate?: Date;
 };
 
 const readJsonFile = (pathToPackage: string): any => {
-    const fileContent = fs.readFileSync(
-      path.join(__dirname, pathToPackage),
-      "utf8"
-    );
-    return JSON.parse(fileContent);
-  
-}
+  const fileContent = fs.readFileSync(
+    path.join(__dirname, pathToPackage),
+    "utf8"
+  );
+  return JSON.parse(fileContent);
+};
+
+const parseDependencies = (packageJson: any): PackageInfo[] => {
+  const packages = [];
+
+  packages.push(
+    ...Object.entries(packageJson.dependencies).map(([name, version]) => ({
+      name,
+      isDevDependency: false,
+      currentVersion: version,
+    }))
+  );
+
+  packages.push(
+    ...Object.entries(packageJson.devDependencies).map(([name, version]) => ({
+      name,
+      isDevDependency: true,
+      currentVersion: version,
+    }))
+  );
+
+  return packages;
+};
 
 export const myPackage = (pathToPackage: string): PackageInfo[] => {
   const packageJson = readJsonFile(pathToPackage);
   // extract name and currentVersion
-  const json = JSON.parse(packageJson);
-  const dependencyAnalalysis = json.dependenies.map((item) => {return {name: item.}})
-  
+  console.log(packageJson);
+  const parsedDependencies = parseDependencies(packageJson);
+  console.log(parsedDependencies);
   // iterate and fetch additional info
-  return [];
+  return parsedDependencies;
 };
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -5,30 +5,15 @@ const PATH_TO_MOCK_PACKAGE = "../test/mock-package.json";
 
 describe("index", () => {
   describe("myPackage", () => {
-    it("should return an array with name and version", () => {
+    it("should return an array with name, version and isDevDependency", () => {
       const result = myPackage(PATH_TO_MOCK_PACKAGE);
 
       expect(result).toMatchObject([
-        {
-          name: "react",
-          version: "^17.0.1",
-        },
-        {
-          name: "moment",
-          version: "^2.20.1",
-        },
-        {
-          name: "typescript",
-          version: "^4.2.4",
-        },
-        {
-          name: "eslint",
-          version: "^7.25.0",
-        },
-        {
-          name: "jest",
-          version: "^27.2.0",
-        },
+        { name: "react", isDevDependency: false, currentVersion: "^17.0.1" },
+        { name: "moment", isDevDependency: false, currentVersion: "^2.20.1" },
+        { name: "typescript", isDevDependency: true, currentVersion: "^4.2.4" },
+        { name: "eslint", isDevDependency: true, currentVersion: "^7.25.0" },
+        { name: "jest", isDevDependency: true, currentVersion: "^27.2.0" },
       ]);
     });
   });

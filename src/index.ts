3d9a2612a64bd77cdde3e0ad090cc0a034341aea
Jonathan Wagner <jonathanw@theodo.co.uk>
2023-02-20 17:01:27 +0000

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-export const myPackage = (taco = ''): string => `${taco} from my package`;
+import * as fs from "fs";
+import * as path from "path";
+
+type PackageInfo = {
+  name: string;
+  isDevDependency: boolean;
+  currentVersion: string;
+  latestVersion: string;
+  latestVersionDate: Date;
+  currentVersionDate: Date;
+};
+
+const readJsonFile = (pathToPackage: string): any => {
+    const fileContent = fs.readFileSync(
+      path.join(__dirname, pathToPackage),
+      "utf8"
+    );
+    return JSON.parse(fileContent);
+  
+}
+
+export const myPackage = (pathToPackage: string): PackageInfo[] => {
+  const packageJson = readJsonFile(pathToPackage);
+  // extract name and currentVersion
+  const json = JSON.parse(packageJson);
+  const dependencyAnalalysis = json.dependenies.map((item) => {return {name: item.}})
+  
+  // iterate and fetch additional info
+  return [];
+};
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,13 +1,35 @@
-import { myPackage } from '../src';
+import { myPackage } from "../src";
 
-describe('index', () => {
-  describe('myPackage', () => {
-    it('should return a string containing the message', () => {
-      const message = 'Hello';
+// This is from the src/index.ts file
+const PATH_TO_MOCK_PACKAGE = "../test/mock-package.json";
 
-      const result = myPackage(message);
+describe("index", () => {
+  describe("myPackage", () => {
+    it("should return an array with name and version", () => {
+      const result = myPackage(PATH_TO_MOCK_PACKAGE);
 
-      expect(result).toMatch(message);
+      expect(result).toMatchObject([
+        {
+          name: "react",
+          version: "^17.0.1",
+        },
+        {
+          name: "moment",
+          version: "^2.20.1",
+        },
+        {
+          name: "typescript",
+          version: "^4.2.4",
+        },
+        {
+          name: "eslint",
+          version: "^7.25.0",
+        },
+        {
+          name: "jest",
+          version: "^27.2.0",
+        },
+      ]);
     });
   });
 });

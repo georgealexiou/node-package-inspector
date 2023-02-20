60836c5eaf1e70ba9e17d0b4251553393e894221
Jonathan Wagner <jonathanw@theodo.co.uk>
2023-02-20 22:32:26 +0000

    mock child_process

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -2,6 +2,11 @@ import { myPackage } from "../src";
 
 // This is from the src/index.ts file
 const PATH_TO_MOCK_PACKAGE = "../test/mock-package.json";
+const FAKE_DATE = "2023-02-11T20:31:23.629Z";
+
+jest.mock("child_process", () => ({
+  execSync: () => FAKE_DATE,
+}));
 
 describe("index", () => {
   describe("myPackage", () => {
@@ -13,31 +18,31 @@ describe("index", () => {
           name: "react",
           isDevDependency: false,
           currentVersion: "17.0.1",
-          currentVersionDate: new Date("2023-02-20T19:35:26.034Z"),
+          currentVersionDate: new Date(FAKE_DATE),
         },
         {
           name: "moment",
           isDevDependency: false,
           currentVersion: "2.20.1",
-          currentVersionDate: new Date("2022-11-18T07:54:02.548Z"),
+          currentVersionDate: new Date(FAKE_DATE),
         },
         {
           name: "typescript",
           isDevDependency: false,
           currentVersion: "4.2.4",
-          currentVersionDate: new Date("2023-02-20T07:11:28.226Z"),
+          currentVersionDate: new Date(FAKE_DATE),
         },
         {
           name: "eslint",
           isDevDependency: false,
           currentVersion: "7.25.0",
-          currentVersionDate: new Date("2023-02-11T20:31:23.629Z"),
+          currentVersionDate: new Date(FAKE_DATE),
         },
         {
           name: "jest",
           isDevDependency: false,
           currentVersion: "27.2.0",
-          currentVersionDate: new Date("2023-02-15T11:57:49.016Z"),
+          currentVersionDate: new Date(FAKE_DATE),
         },
       ]);
     });
